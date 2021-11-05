30b07be80d56135c312352210875de7def28988b
OdinStorm <51592303+OdinStorm@users.noreply.github.com>
2021-11-05 16:04:24 +0700

    update js file

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
-        if($(window).width() < 863){
+
+        var windowsize = $(window).width();
+
+        $(window).resize(function() {
+            var windowsize = $(window).width();
+        });
+
+        if(windowsize < 863){
             $("nav").hide();
+        }
+
+        if(windowsize < 1248){
+            $("img.summary-pic").hide();
+        }
+
+        if(windowsize >= 863){
+            $("nav").show();
         }
     }
 );
